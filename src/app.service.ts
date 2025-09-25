00e9f568d6dcabfb173485ae6e86fdd77c8bde31
Rakshit Sejpal <rakshit@medianv.com>
2025-09-25 11:50:02 +0530

    remove unnecessary code

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,4 @@
 import { Injectable } from '@nestjs/common';
-// import { Bucket, Cluster, Collection, connect, ConnectOptions, DocumentNotFoundError, GetResult } from 'couchbase';
-
-import {
-    Bucket,
-    Cluster,
-    Collection,
-    connect,
-    ConnectOptions,
-    DocumentNotFoundError,
-    GetResult,
-} from 'couchbase'
 
 @Injectable()
 export class AppService {
